9ae07c70ae06779eeb65be08f13bf7ae8993ebaa
buteomont@gmail.com <buteomont@gmail.com>
2021-07-03 06:30:47 -0500

    Added OTA update feature

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,8 @@
 #include <PubSubClient.h> 
 #include <EEPROM.h>
 #include <pgmspace.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 #include "runLimiter.h"
 
@@ -197,6 +199,52 @@ boolean sendMessage(char* topic, char* value)
   return success;
   }
 
+void otaSetup()
+  {
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() 
+    {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+    });
+
+    ArduinoOTA.onEnd([]() {
+      Serial.println("\nEnd");
+    });
+
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+
+    ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+  }
 
 void setup() 
   {
@@ -249,7 +297,10 @@ void setup()
     connectToWiFi(); //connect to the wifi
   
   if (WiFi.status() == WL_CONNECTED)
+    {
     sendMessage(MQTT_TOPIC_STATUS, settings.mqttRunMessage); //running!
+    otaSetup(); //initialize the OTA stuff
+    }
   }
 
 void loop()
@@ -258,7 +309,8 @@ void loop()
   static unsigned long lastTime=0;
   mqttClient.loop(); //This has to happen every so often or we get disconnected for some reason
   checkForCommand(); // Check for input in case something needs to be changed to work
-  
+  ArduinoOTA.handle(); //Check for new version
+
   if (settings.debug && millis()%1000==0 && !wrapped) //not too fast
     {
     unsigned long remainingTime=timeoutCount-millis();
@@ -277,7 +329,6 @@ void loop()
     connectToWiFi(); //make sure we're connected to the broker
     timeoutMessageSent=sendMessage(MQTT_TOPIC_STATUS, settings.mqttTimeoutMessage);
     digitalWrite(RELAY_PORT,RELAY_OFF);
-//    digitalWrite(LED_BUILTIN,LED_ON);
     delay(1000); //repeat every second until message is sent
     }
   }
@@ -304,7 +355,7 @@ boolean connectToWiFi()
     WiFi.mode(WIFI_STA); //station mode, we are only a client in the wifi world
     WiFi.begin(settings.ssid, settings.wifiPassword);
 
-    //try for a few seconds to connect to existing wifi
+    //try for a few seconds to connect to wifi
     for (int i=0;i<WIFI_CONNECTION_ATTEMPTS;i++)  
       {
       if (WiFi.status() == WL_CONNECTED)
@@ -318,6 +369,7 @@ boolean connectToWiFi()
 
     if (WiFi.status() == WL_CONNECTED)
       {
+      digitalWrite(LED_BUILTIN,LED_ON); //show we're connected
       if (settings.debug)
         {
         Serial.println(F("Connected to network."));
@@ -333,6 +385,7 @@ boolean connectToWiFi()
       Serial.print("Wifi status is ");
       Serial.println(WiFi.status());
       Serial.println(F("WiFi connection unsuccessful."));
+      digitalWrite(LED_BUILTIN,LED_OFF); //stay off until we connect
       }
     }
   if (WiFi.status() == WL_CONNECTED)
@@ -460,7 +513,9 @@ void showSettings()
   Serial.print("MQTT client ID=<automatically generated client ID> (");
   Serial.print(settings.mqttClientId);
   Serial.println(") **Use \"resetmqttid=yes\" to regenerate");
-  Serial.println("\n*** Use \"factorydefaults=yes\" to reset all settings ***\n");
+  Serial.println("\n*** Use \"factorydefaults=yes\" to reset all settings ***");
+  Serial.print("\nIP Address=");
+  Serial.println(WiFi.localIP());
   }
 
 /*
